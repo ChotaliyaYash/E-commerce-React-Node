72a95340aae13dcbc283e0b7bc4853b50e5b1887
ChotaliyaYash <yashkumar.c@ahduni.edu.in>
2023-12-18 10:52:37 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,12 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 // const dotenv = require('dotenv');
+const path = require('path');
 
 const errorMiddleware = require('./middlewares/errors')
 
 // Setting up config file 
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config()
+require('dotenv').config()
 // dotenv.config({ path: 'backend/config/config.env' })
 
 app.use(express.json());
@@ -33,11 +34,14 @@ app.use('/api/v1', order)
 if (process.env.NODE_ENV === 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    // })
+
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
-
 
 // Middleware to handle errors
 app.use(errorMiddleware);
